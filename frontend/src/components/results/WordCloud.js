90aa73f473833257604686b366bf2e5cec599b33
Kazuto Okamoto <kazutookamoto@doi-mbp.local>
2020-02-25 15:13:52 +0900

    add dialog for explanation about wordcloud

--- frontend/src/components/results/WordCloud.js
+++ frontend/src/components/results/WordCloud.js
@@ -1,24 +1,76 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+import { Typography, Grid, Button, Dialog, DialogTitle, DialogContent, DialogContentText } from "@material-ui/core";
 
-function WordCloud({encodedWordcloud}) {
-  return (
-    <div>
-      {
-        encodedWordcloud !== null ?
-          <div>
-            <Typography variant='h5'>ワードクラウド</Typography>
-            <img 
-              src={"data:image/png;base64," + encodedWordcloud}
-              alt="wordcloud"
-              width="500" height="500"
-              style={{ marginTop: 20 }}
-            />
-          </div>
-        : null
-      }
-    </div>
-  )
+class WordCloud extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    }
+
+    this.handleOpen = this.handleOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  };
+
+  handleOpen() {
+    this.setState({ open: true });
+  }
+
+  handleClose() {
+    this.setState({ open: false });
+  }
+
+  render() {
+    return (
+      <div>
+        {
+          this.props.encodedWordcloud !== null ?
+            <div>
+              <Grid container>
+                <Grid item xs={4}>
+                  <Typography variant='h5'>ワードクラウド</Typography>
+                </Grid>
+                <Grid item xs={4}>
+                  <Button 
+                    size='large'
+                    onClick={this.handleOpen}
+                  >
+                    ワードクラウドとは？
+                  </Button>
+                </Grid>
+                <Grid item xs={4}>
+                </Grid>
+              </Grid>
+              <img 
+                src={"data:image/png;base64," + this.props.encodedWordcloud}
+                alt="wordcloud"
+                width="500" height="500"
+                style={{ marginTop: 20 }}
+              />
+            </div>
+          : null
+        }
+        <Dialog
+          aria-labelledby="modal-title"
+          aria-describedby="modal-description"
+          open={this.state.open}
+          onClose={this.handleClose}
+        >
+          <DialogTitle>
+            ワードクラウドとは？
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              文章内の単語の頻度を視覚的に捉えやすくしたものです。              
+            </DialogContentText>
+            <DialogContentText>            
+              頻度の高い単語ほど大きく表示されます。
+            </DialogContentText>
+          </DialogContent>
+        </Dialog>
+      </div>
+    )
+  }
 }
 
 export default WordCloud;
